b410fc78c7947b703300f727f0642838013e1c07
Datnk-hust <datnk@telemed.vn>
2020-04-06 13:14:43 +0700

    aaaaaaaaaaaaaaaaaaaaaaaassa

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-         Eloquent::unguard();
-        $this->call(UsersTableSeeder::class);
+        //  Eloquent::unguard();
+        // $this->call(UsersTableSeeder::class);
         $this->call(LevelTableSeed::class);
 
     }
