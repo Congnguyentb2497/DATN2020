05b5e3036f2eba84ee367acdfebc115ad672f930
Datnk-hust <datnk@telemed.vn>
2020-04-21 18:14:08 +0700

    alola

--- resources/views/admin/addDep.blade.php
+++ resources/views/admin/addDep.blade.php
@@ -41,7 +41,7 @@
         </td>
       </tr>
       <tr>
-        <td colspan="2"><div class="canl"><a style="text-decoration: none; color: white;" href="{{route('show.department')}}" >Hủy</a></div></td>
+        <td colspan="2" style="text-align: center;"><div class="canl"><a style="text-decoration: none; color: white;" href="{{route('show.department')}}" >Hủy</a></div></td>
       </tr>
     </table>
     <br>
