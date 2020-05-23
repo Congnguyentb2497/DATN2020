0882e1f563e79e17ea3a278823b25b7e52a58648
Datnk-hust <datnk@telemed.vn>
2020-05-24 00:16:29 +0700

    cvdb

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,9 +5,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <style type="text/css">
-	body{
+body{
   font-family: TimesNewRoman, "Times New Roman", Times, Baskerville, Georgia, serif;
-  background-image: url(./public/images/nen.png);
+  background-image: url(./asset/nen.png);
   margin: 0 auto 0 auto;  
   width:100%; 
   text-align:center;
@@ -33,7 +33,7 @@ h1{
   border: #2980b9 4px solid; 
   margin-left: 550px;
   margin-top: 370px;
-  height: 365px;
+  height: 360px;
   
 }
 
