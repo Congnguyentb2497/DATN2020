a94f62dbaaa604673b072bdfd7b8b30678387b9e
Datnk-hust <datnk@telemed.vn>
2020-05-30 01:43:31 +0700

    bdbkaaaaaddaadddaaajdaadaadddkla

--- resources/views/ktv/device/schedule.blade.php
+++ resources/views/ktv/device/schedule.blade.php
@@ -15,7 +15,7 @@
   	margin-left: 40px;
   }
   .form{
-  	margin-left: 50px;
+  	margin-left: 40px;
   	font-size: 20px;
   }
 </style>
@@ -38,15 +38,15 @@
  	<form class="form">
   <div class="form-group">
     <label for="exampleInputEmail1">Hoạt động bảo dưỡng</label>
-    <input style="width: 80%" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nhập hoạt động cần bảo dưỡng">
+    <input style="width: 90%" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nhập hoạt động cần bảo dưỡng">
     <small id="emailHelp" class="form-text text-muted">VD: Kiểm tra buồng kính chiếu tia X</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Tần suất thực hiện</label>
-    <input style="width: 80%" type="password" class="form-control" id="exampleInputPassword1" >
+    <input style="width: 90%" type="password" class="form-control" id="exampleInputPassword1" >
     <small id="emailHelp" class="form-text text-muted">VD: hàng tuần, hàng tháng,... </small>
   </div>
-  <button type="submit" class="btn btn-primary">Lưu</button>
+  <button style="width: 70px;" type="submit" class="btn btn-primary">Lưu</button>
 </form>
 </div>
 @endsection
