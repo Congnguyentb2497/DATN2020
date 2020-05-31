1b2ecdadf2d34c3d77c5d0c7415cd278db5eb92a
Datnk-hust <datnk@telemed.vn>
2020-06-01 00:16:47 +0700

    addddddddgkddkdkd

--- resources/views/doctor/listDevice.blade.php
+++ resources/views/doctor/listDevice.blade.php
@@ -142,7 +142,7 @@
         <td>{{$row->dv_id}}</td>
         <td>{{$row->dv_name}}</td>
         <td>{{ $row->dv_model}}</td>
-        <td>{{ \App\Device_type::where(['id'=>$row->dv_type_id])->pluck('dv_type_name')->first() }}</td>
+        <td>{{ \App\Device_type::where(['dv_type_id'=>$row->dv_type_id])->pluck('dv_type_name')->first() }}</td>
         <td>{{$row->handover_date}}</td>
         <td>{{ $row->maintain_date }}</td>
         <td style="text-align: center;">
