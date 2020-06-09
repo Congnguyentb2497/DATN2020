ce58a3e8d8bd15a711cfcd0ac033e0d86fdac1be
datnk-hust <datnk@telemed.vn>
2020-06-09 23:20:05 +0700

    check tao bang21111

--- resources/views/ktv/device/maintain_check.blade.php
+++ resources/views/ktv/device/maintain_check.blade.php
@@ -114,11 +114,8 @@ $month = [
             @if($row->id.$i == $ch->check.id)
             <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;">{{$ch->type_check}}</button>
           </td>
-            @else
-            <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;"></button>
-          </td>
             @endif
-          @endfor
+          @endforeach
         @else
             <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;"></button>
           </td>
