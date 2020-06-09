a4d6eaa108f754fc7ade318de60783615d4c1797
datnk-hust <datnk@telemed.vn>
2020-06-09 23:38:22 +0700

    check tao bang211110110

--- resources/views/ktv/device/maintain_check.blade.php
+++ resources/views/ktv/device/maintain_check.blade.php
@@ -111,7 +111,7 @@ $month = [
       @for($i = 1; $i <= 53; $i++)
         @if($checked != null)
          @foreach($checked as $ch)
-            @if($row->id.$i == $ch->check_id.id)
+            @if($row->id.$i == $ch->check_id)
             <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;">{{$ch->type_check}}</button>
           </td>
             @endif
