fee4242098b9a317622f90d380302393eb6717a5
datnk-hust <datnk@telemed.vn>
2020-06-09 23:23:44 +0700

    check tao bang211110

--- resources/views/ktv/device/maintain_check.blade.php
+++ resources/views/ktv/device/maintain_check.blade.php
@@ -119,6 +119,7 @@ $month = [
         @else
             <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;"></button>
           </td>
+        @endif
       @endfor
   </tr>
     @endforeach
