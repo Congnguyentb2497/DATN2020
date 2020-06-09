d2ff301e318e68fb4279612030a6b3758fa56d25
datnk-hust <datnk@telemed.vn>
2020-06-10 00:48:22 +0700

    check tao1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -826,9 +826,9 @@ public function showmaintain(Request $request){
     public function maintainCheck($id){
         $dev = DB::table('device')->where('dv_id',$id)->first();
         $dv = DB::table('schedule_action')->where('dv_id',$id)->get();
-        $ch = DB::table('check')->where('dv_id',$id)->get();
-
-        return view('ktv.device.maintain_check')->with(['device'=>$dev,'maintainAct'=>$dv,'checked'=>$ch]);
+        $ch = DB::table('check')->where('dv_id',$id)->groupBy('act_id');
+        dd($ch);
+        //return view('ktv.device.maintain_check')->with(['device'=>$dev,'maintainAct'=>$dv,'checked'=>$ch]);
     }
 
     public function checked(Request $request, $id){
