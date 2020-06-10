b37328fdd91d6520ab4f20409ac592dca1faaa85
datnk-hust <datnk@telemed.vn>
2020-06-10 08:36:35 +0700

    file check

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -827,8 +827,8 @@ public function showmaintain(Request $request){
         $dev = DB::table('device')->where('dv_id',$id)->first();
         $dv = DB::table('schedule_action')->where('dv_id',$id)->get();
         $ch = DB::table('check')->where('dv_id',$id)->groupBy('act_id');
-        dd($ch);
-        //return view('ktv.device.maintain_check')->with(['device'=>$dev,'maintainAct'=>$dv,'checked'=>$ch]);
+        //dd($ch);
+        return view('ktv.device.maintain_check')->with(['device'=>$dev,'maintainAct'=>$dv,'checked'=>$ch]);
     }
 
     public function checked(Request $request, $id){
--- resources/views/ktv/device/maintain_check.blade.php
+++ resources/views/ktv/device/maintain_check.blade.php
@@ -108,25 +108,18 @@ $month = [
     @foreach($maintainAct as $row)
     <tr>
       <td>{{ $row -> scheduleAct}}</td>
-      
-      @if($checked != null) 
-        @foreach($checked as $ch)
-          @for($i = 1; $i <= 53; $i++)
-            @if($ch->check_id == $row->id.$i)
-            <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;font-size: 12px;">{{$ch->type_check}}</button>
-            </td>
-            @else
-              <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;"></button>
-              </td>
+       @for($i = 1; $i <= 53; $i++)
+      <td style="text-align: center; width: 1.5%;cursor: pointer;"> 
+        <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;font-size: 12px;">
+           @if($checked != null) 
+            @foreach($checked as $ch)
+                @if($ch->check_id == $row->id.$i)
+                    {{$ch->type_check}}
+                @endif
+            @endforeach
             @endif
-          @endfor
-        @endforeach
-      @else
-        @for($i = 1; $i <= 53; $i++)
-        <td style="text-align: center; width: 1.5%;cursor: pointer;"> <button data-deviceid="{{ $row->id.$i   }}" id="{{ $row->id.$i }}" onmousemove="show()" class="check" style="height: 20px;"></button>
-              </td>
-        @endfor
-      @endif
+        </button>
+      </td>
     </tr>
     @endforeach
     @endif
