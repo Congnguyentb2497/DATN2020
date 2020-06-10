c84a3a78f8702a231ac30925e3d07e8b86bcca37
datnk-hust <datnk@telemed.vn>
2020-06-10 17:23:16 +0700

    file check91231111111

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -824,9 +824,15 @@ public function showmaintain(Request $request){
     }
 
     public function maintainCheck(Request $request,$id){
+        if($request->year){
+            $y = $request->year;
+            $ch = DB::table('check')->where('year',$y)->where('dv_id',$id)->get();
+        }else{
+            $ch = DB::table('check')->where('dv_id',$id)->get();
+        }
         $dev = DB::table('device')->where('dv_id',$id)->first();
         $dv = DB::table('schedule_action')->where('dv_id',$id)->get();
-        $ch = DB::table('check')->->where('year',$request->year)->where('dv_id',$id)->get();
+        
         //dd($ch);
         return view('ktv.device.maintain_check')->with(['device'=>$dev,'maintainAct'=>$dv,'checked'=>$ch]);
     }
