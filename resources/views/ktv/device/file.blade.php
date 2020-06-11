4968a0485bc951a2e890b407fe4c7a7aff71b5e8
datnk-hust <datnk@telemed.vn>
2020-06-12 01:42:32 +0700

    printpdf900010001108

--- resources/views/ktv/device/file.blade.php
+++ resources/views/ktv/device/file.blade.php
@@ -8,7 +8,8 @@
 </style>
 <h2>Hồ Sơ Thiết Bị: {{ \App\Device::where(['id' => $dv])->pluck('dv_name')->first()}}</h2>
 <h3>Mã thiết bị: {{ \App\Device::where(['id' => $dv])->pluck('dv_id')->first()}}</h3>
-<div style="width: 300px;padding: 5px; background-color: green;text-align: center; margin-left: 40px;border-radius: 5px;" ><a style="text-decoration: none;color: black;" href="{{ route('device.maintainCheck',['id'=>$dv_id])}}">Xem lịch xử bảo dưỡng thiết bị</a></div>
+<div style="width: 300px;padding: 5px;float: left; background-color: green;text-align: center; margin-left: 40px;border-radius: 5px;" ><a style="text-decoration: none;color: black;" href="{{ route('device.maintainCheck',['id'=>$dv_id])}}">Xem lịch xử bảo dưỡng thiết bị</a></div>
+<div style="width: 300px;padding: 5px;float: left; background-color: yellow;text-align: center; margin-left: 40px;border-radius: 5px;" ><a style="text-decoration: none;color: black;" href="{{ route('device.getEdit',['id'=>$dv])}}">Thông tin chi tiết thiết bị</a></div>
 <div class="container2">
   <br>
   <table class="table table-condensed table-bordered table-hover">
